faef8eb8efeec4dfdbebf9460160aa43731782a7
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-04-01 15:12:17 +0100

    working on routes

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,13 @@ import {config} from "dotenv";
 
 config();
 const app = express();
+const morgan = require("morgan");
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
 
 // Get all restaurants
-app.get("/api/v1/restaurants", (req: express.Request, res: express.Response) => {
+app.get("/api/v1/restaurants", (req: express.Request, res: express.Response) => {    
     res.status(200).json({
         status: "success",
         data: {
@@ -24,6 +28,17 @@ app.get("/api/v1/restaurants/:id", (req: express.Request, res: express.Response)
             restaurant: `Restaurant with ID: ${id}`
         }
     });
+});
+
+// Create a restaurant
+app.post("/api/v1/restaurants", (req: express.Request, res: express.Response) => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "Restaurant created"
+        }
+    });
+    console.log(req.body);
 });
 
 const port = process.env.PORT || 3001;
