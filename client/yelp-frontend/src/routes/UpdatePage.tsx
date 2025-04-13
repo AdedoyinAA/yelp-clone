113747cc027a1837e575bea44cc001c1f556d142
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-04-13 16:30:55 +0100

    codegen

--- client/yelp-frontend/src/routes/UpdatePage.tsx
+++ client/yelp-frontend/src/routes/UpdatePage.tsx
@@ -4,7 +4,7 @@ import UpdateRestaurant from '../components/UpdateRestaurant'
 const UpdatePage = () => {
   return (
     <div>
-      <h1 className='text-center my-5'>Update Restaurant</h1>
+      <h1 className='text-center display-1 my-5'>Update Restaurant</h1>
       <UpdateRestaurant/>
     </div>
   )
